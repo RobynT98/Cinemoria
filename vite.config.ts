174f4806b1d7b32f0d6cb0a2db3332df02ffe519
Conri Turesson <50335159+RobynT98@users.noreply.github.com>
2025-10-18 22:36:53 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Ändra detta om repo-namnet inte är "cinemoria".
+const repoBase = '/cinemoria/'
+
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173
-  }
+  base: repoBase, // <-- viktigt för GitHub Pages
+  server: { port: 5173 }
 })
