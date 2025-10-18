77c8207d9015b5435adcd1a09a8c5048bcc1c6dd
Conri Turesson <50335159+RobynT98@users.noreply.github.com>
2025-10-18 23:37:44 +0200

    Update AddPage.tsx

--- src/pages/AddPage.tsx
+++ src/pages/AddPage.tsx
@@ -5,7 +5,13 @@ export default function AddPage() {
   return (
     <section className="p-4">
       <h1 className="text-2xl font-semibold mb-3">Lägg till film</h1>
-      <MovieForm onSubmit={async (data) => { await addMovie(data); alert('Sparad!'); }} />
+      <MovieForm
+        submitLabel="Spara film"
+        onSubmit={async (data) => {
+          await addMovie(data)
+          alert('Sparad!')
+        }}
+      />
     </section>
   )
 }
