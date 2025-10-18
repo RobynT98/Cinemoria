df8cd7bdb1c52293d43d1f300c75e3ad01409f4e
Conri Turesson <50335159+RobynT98@users.noreply.github.com>
2025-10-18 22:32:56 +0200

    Create src/pages/CollectionsPage.tsx

---
+++ src/pages/CollectionsPage.tsx
@@ -0,0 +1,8 @@
+export default function CollectionsPage() {
+  return (
+    <section className="p-4">
+      <h1 className="text-2xl font-semibold mb-3">Samlingar</h1>
+      <p className="text-sand-300">Skapa listor som ”Vampyrfilm”, ”Favoriter 2025” m.m. (kommer snart).</p>
+    </section>
+  )
+}
