f91d8879f6f823243ec3ca66f04ba0e37581db87
Conri Turesson <50335159+RobynT98@users.noreply.github.com>
2025-10-18 22:56:14 +0200

    Update AddPage.tsx

--- src/pages/AddPage.tsx
+++ src/pages/AddPage.tsx
@@ -1,8 +1,11 @@
+import MovieForm from '@/components/MovieForm'
+import { addMovie } from '@/db'
+
 export default function AddPage() {
   return (
     <section className="p-4">
       <h1 className="text-2xl font-semibold mb-3">Lägg till film</h1>
-      <p className="text-sand-300">I nästa steg bygger vi formuläret (titel, år, poster, genrer, taggar m.m.).</p>
+      <MovieForm onSubmit={async (data) => { await addMovie(data); alert('Sparad!'); }} />
     </section>
   )
 }
