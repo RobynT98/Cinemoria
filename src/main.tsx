db804320df1a8585bf7c15e39c61768a66bbd3f3
Conri Turesson <50335159+RobynT98@users.noreply.github.com>
2025-10-18 23:14:43 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,13 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
+// Vite ger oss base som t.ex. "/Cinemoria/"
+// React Router vill ha den utan avslutande slash.
+const basename = (import.meta.env.BASE_URL || '/').replace(/\/$/, '')
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
