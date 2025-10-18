9e6c2a9ff8a1788fe864f5cb3ad8b1c64717bead
Conri Turesson <50335159+RobynT98@users.noreply.github.com>
2025-10-19 01:31:22 +0200

    Update AddPage.tsx

--- src/pages/AddPage.tsx
+++ src/pages/AddPage.tsx
@@ -1,17 +1,21 @@
+// src/pages/AddPage.tsx
 import MovieForm from '@/components/MovieForm'
+import type { Movie } from '@/db'
 import { addMovie } from '@/db'
 
 export default function AddPage() {
+  async function handleSubmit(data: Movie) {
+    await addMovie(data)
+    alert('Sparad!')
+  }
+
   return (
     <section className="p-4">
       <h1 className="text-2xl font-semibold mb-3">Lägg till film</h1>
-      <MovieForm
-        submitLabel="Spara film"
-        onSubmit={async (data) => {
-          await addMovie(data)
-          alert('Sparad!')
-        }}
-      />
+
+      <div className="card p-4">
+        <MovieForm submitLabel="Spara film" onSubmit={handleSubmit} />
+      </div>
     </section>
   )
 }
