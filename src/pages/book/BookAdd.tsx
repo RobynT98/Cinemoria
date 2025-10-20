8e17b3f26304ad8da3e983995becffdf965ef9ed
Conri Turesson <50335159+RobynT98@users.noreply.github.com>
2025-10-20 10:54:03 +0200

    Update BookAdd.tsx

--- src/pages/book/BookAdd.tsx
+++ src/pages/book/BookAdd.tsx
@@ -1,22 +1,21 @@
-// src/pages/book/BookAdd.tsx
+import { useNavigate } from "react-router-dom";
 import BookForm from "@/components/BookForm";
 import { addBook } from "@/db";
-import { useNavigate } from "react-router-dom";
+import type { Book } from "@/db";
 
 export default function BookAdd() {
   const navigate = useNavigate();
 
+  async function handleSubmit(data: Book) {
+    await addBook(data);
+    alert("Boken sparad!");
+    navigate("/book");
+  }
+
   return (
     <section className="p-4">
       <h1 className="text-2xl font-semibold mb-3">Lägg till bok</h1>
-      <BookForm
-        submitLabel="Spara bok"
-        onSubmit={async (data) => {
-          await addBook(data);
-          alert("Bok sparad!");
-          navigate("/book");
-        }}
-      />
+      <BookForm submitLabel="Spara bok" onSubmit={handleSubmit} />
     </section>
   );
 }
