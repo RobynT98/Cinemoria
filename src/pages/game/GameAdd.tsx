61f7a9c805387bfa3fda24395ec36d868e865e24
Conri Turesson <50335159+RobynT98@users.noreply.github.com>
2025-10-20 10:54:34 +0200

    Update GameAdd.tsx

--- src/pages/game/GameAdd.tsx
+++ src/pages/game/GameAdd.tsx
@@ -1,18 +1,21 @@
-// src/pages/game/GameAdd.tsx
+import { useNavigate } from "react-router-dom";
 import GameForm from "@/components/game/GameForm";
 import { addGame } from "@/db";
+import type { Game } from "@/db";
 
 export default function GameAdd() {
+  const navigate = useNavigate();
+
+  async function handleSubmit(data: Game) {
+    await addGame(data);
+    alert("Spelet sparat!");
+    navigate("/game");
+  }
+
   return (
     <section className="p-4">
       <h1 className="text-2xl font-semibold mb-3">Lägg till spel</h1>
-      <GameForm
-        submitLabel="Spara spel"
-        onSubmit={async (data) => {
-          await addGame(data);
-          alert("Sparat!");
-        }}
-      />
+      <GameForm submitLabel="Spara spel" onSubmit={handleSubmit} />
     </section>
   );
 }
